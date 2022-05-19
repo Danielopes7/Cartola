a8438cbdfccf9b17ce0fe02ae9c0369bcf9ff2cf
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-05-19 15:59:14 -0300

    feat: new controller/migration/model Jogadores

---
+++ app/Http/Controllers/JogadoresController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jogadores;
+use Illuminate\Http\Request;
+
+class JogadoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Jogadores  $jogadores
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Jogadores $jogadores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Jogadores  $jogadores
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Jogadores $jogadores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Jogadores  $jogadores
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Jogadores $jogadores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Jogadores  $jogadores
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Jogadores $jogadores)
+    {
+        //
+    }
+}
