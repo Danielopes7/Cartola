af29e10b6126c372ac42cfd2621ce5e8bd06e443
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-05-20 19:55:58 -0300

    fix: web routes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::Resource('jogador', 'App\Http\Controllers\JogadorController');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', 'JogadorController@index');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
